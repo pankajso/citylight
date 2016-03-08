6ed989f442e51478a76b356dbe29eace5c61a67d
Pankaj Sonawane <pankajso@gmail.com>
2016-03-08 17:30:33 +0530

    Add or update fittings in infrastructure

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -3,7 +3,13 @@ class InstallationsController < ApplicationController
   end
 
   def create
-    @inst = Infrastructure.find(params[:infrastructure_id]).installations.create(fitting_id: params[:typo], quantity: params[:quantity], running_time: params[:running_time] )
-    # @installations = Infrastructure.find(params[:infrastructure_id]).installations
+    @inst = Infrastructure.find(params[:infrastructure_id]).installations.find_by(fitting_id: params[:typo])
+    if @inst.present?
+      @inst.quantity = params[:quantity]
+      @inst.running_time = params[:running_time]
+      @inst.save!
+    else
+      @inst = Infrastructure.find(params[:infrastructure_id]).installations.create(fitting_id: params[:typo], quantity: params[:quantity], running_time: params[:running_time] )
+    end
   end
 end
--- app/views/installations/create.js.coffee
+++ app/views/installations/create.js.coffee
@@ -1,8 +1,17 @@
 <% if @inst.valid? %>
 newrow = '<%= j(render @inst) %>'
+tblfind = '<%= @inst.fitting_id %>'
+findrow = $( ".tbl" ).hasClass("tbl-<%= @inst.fitting_id %>")
+if findrow is true
+  $( ".tbl tbl-<%= @inst.fitting_id %>" ).css( "background-color", "red" )
+  row = $( ".tbl.tbl-<%= @inst.fitting_id %>" ).last()
+  row.remove()
+
 lastrow = $( ".tbl" ).last()
 tble = lastrow.parent()
 tble.append(newrow)
+lastrow = $( ".tbl" ).last()
+lastrow.css( "color", "red" )
 <% else %>
 notice $('.fitting-create #name'), '#ff6666'
 $('.fitting-create #typo').val ''
